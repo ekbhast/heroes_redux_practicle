b27d88aed6b00855712d04b1ab425f5fdd7cb581
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-02-17 16:17:42 +0300

    aaa

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,9 +17,64 @@ export const heroesFetchingError = () => {
     }
 }
 
-export const heroDelete = (id) => {
+export const filtersFetching = () => {
     return {
-        type: 'HERO_DELETE',
+        type: 'FILTERS_FETCHING'
+    }
+}
+
+export const filtersFetched = (filters) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: filters
+    }
+}
+
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
+
+export const activeFilterChanged = (filter) => {
+    return {
+        type: 'ACTIVE_FILTER_CHANGED',
+        payload: filter
+    }
+}
+// export const activeFilterChanged = (filter) => (dipsatch) => {
+//    setTimeout(()=>{
+//     dispatch({
+//         type: 'ACTIVE_FILTER_CHANGED',
+//         payload: filter
+//     })
+//    })
+// }
+
+export const heroCreated = (hero) => {
+    return {
+        type: 'HERO_CREATED',
+        payload: hero
+    }
+}
+
+export const heroDeleted = (id) => {
+    return {
+        type: 'HERO_DELETED',
         payload: id
     }
 }
+
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+	dispatch(filtersFetching());
+	request("http://localhost:3001/filters")
+		.then((data) => dispatch(filtersFetched(data)))
+		.catch(() => dispatch(filtersFetchingError()));
+};
